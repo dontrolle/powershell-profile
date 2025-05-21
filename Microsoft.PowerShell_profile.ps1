2d95d8643843c68173ee299ff208547a4d6fa8fd
Troels Damgaard <troels@nestech.dk>
2025-05-21 10:37:54 +0200

    Improve vs command and import touch

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -35,7 +35,32 @@ function Test-IsElevated
     }
 }
 
+# Various alias's etc (assumes relevant exe's are in PATH)
 function s { Set-Location .. }
+
+function OpenWithVisualStudio ([string] $path) 
+{ 
+  if([string]::IsNullOrEmpty($path))
+    {
+      # is there a single .sln file in the directory(?) - then default to opening that one
+      $slns = Get-ChildItem -filter *.sln
+      if($slns.count -eq 1)
+      {
+        $path = $slns[0]
+      }
+      # on the off chance that there are several sln-files, let's signal this and make sure we open the right one
+      elseif ($slns.count -gt 1){
+        throw "No path given, and several .sln-files in current directory. Please specify which .sln-file to open."
+      }
+    }
+  Start-Process devenv $path 
+}
+
+Set-Alias -Name vs -Value OpenWithVisualStudio
+
+function OpenWithRider ([string] $path) { rider64.exe $path }
+
+Set-Alias -Name rider -Value OpenWithRider
 
 ## Load various utils by me - see https://github.com/dontrolle/Powershell
 
@@ -47,6 +72,7 @@ if ($ImportNvidiaDrivercheck)
 . "c:\src\Powershell\out-clip.ps1"
 . "C:\src\Powershell\Get-FileDefiningFunction.ps1"
 . "C:\src\Powershell\close-vshandles.ps1"
+. "C:\src\Powershell\Update-File.ps1"
 
 ### Posh git stuff
 
